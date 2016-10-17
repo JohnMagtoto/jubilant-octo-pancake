b27ca5cd5224d6a686ddcf7c66543c1003d3125e
JohnMagtoto <johnmagtoto@gmail.com>
2016-10-17 13:21:27 +0900

    added promise

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,3 +5,27 @@ function initMap() {
         zoom: 8
     });
 }
+
+function fetchMapsAPI() {
+    var mapAPI_Promise = new Promise((resolve, reject) => {
+        // var xhr = new XMLHttpRequest() ;
+        // xhr.onreadystatechange = function() {
+        //     if (xhr.readyState == XMLHttpRequest.DONE) {
+
+        //     }
+        // }
+        // xhr.open('GET', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAtMXnr1X63BRhjLDjnNbBXCPLPTwKSaI8', true);
+        // xhr.send(null);
+        if(1 + 1 == 2) {
+            resolve('Promise worked') ;
+        } else {
+            reject('Something is broken in the promise') ;
+        }
+    })
+
+    mapAPI_Promise.then((result)=>{
+        console.log(result) ;
+    }, (reject) => {
+        console.log(reject) ;
+    })
+}
